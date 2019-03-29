7ba2742508e094853c31fe9e58be107180361940
Jorleidis Monsalvo <jp.monsalvo@uniandes.edu.co>
2019-03-28 22:05:46 -0500

    correccion rutas prestamo

--- src/resources/prestamo.js
+++ src/resources/prestamo.js
@@ -4,10 +4,10 @@ const {jwtMW}= require('../config');
 module.exports = function(app) {
 	var c = new Controller(require('../models/prestamoModel'), './data/prestamos.json', ['userID', 'prestamoID'],'objectId');
 
-	app.get('/users/:userID/objetos/:objectID/prestamos',function(req, res)  {
+	app.get('/users/:userID/prestamos',function(req, res)  {
 		c.list_all(req, res);
 	})
-	app.post('/users/:userID/objetos/:objectID/prestamos',function(req, res)  {
+	app.post('/users/:userID/prestamos',function(req, res)  {
 		c.list_all(req, res);
 	})
 	app.get('/users/:userID/prestamos/:prestamoID',function(req, res)  {
