f269101e74e48c4f5395065c4dcd5a782df53ba3
DESKTOP\User <jp.monsalvo@uniandes.edu.co>
2019-04-22 23:33:08 -0500

    Prestamos

--- src/models/prestamoModel.js
+++ src/models/prestamoModel.js
@@ -3,37 +3,36 @@ const Extension = require('joi-date-extensions');
 const Joi = BaseJoi.extend(Extension);
 const fs = require('fs');
 
-exports.createSchema = { 
+exports.createSchema = {
     id: Joi.string().optional(),
     paymentId: Joi.string().optional(),
     userId: Joi.string().required(),
     objectId: Joi.string().required(),
     startDate: Joi.string().required(),
-    endDate: Joi.string().required(), 
-    valor: Joi.number().required()
+    endDate: Joi.string().required(),
+    valor: Joi.string().required()
 };
- 
+
 exports.updateSchema = {
-    name: Joi.string().optional(),
     paymentId: Joi.string().optional(),
     userId: Joi.string().optional(),
     objectId: Joi.string().optional(),
     startDate: Joi.string().optional(),
     endDate: Joi.string().optional(),
-    valor: Joi.number().optional()
+    valor: Joi.string().optional()
 
 };
 /*
 * obj = {userID=1, objectID=1, prestamoID= undefined}
 */
-exports.fk_on_list_all = function(obj) {
+exports.fk_on_list_all = function (obj) {
     var file = './data/usuarios.json';
     var data = fs.readFileSync(file, 'utf8');
     var users = JSON.parse(data);
 
-    var usuarioOk=false;
+    var usuarioOk = false;
     for (var i = 0; i < users.length; i++) {
-        if(users[i].id==obj.userID){
+        if (users[i].id == obj.userID) {
             return true;
         }
     }
@@ -46,7 +45,7 @@ Debería comprobar que exista el usuario de id 1, el objeto de id 1
 y que el objeto de id 1 pertenece al usuario de id 1.
 * obj = {userID=1, objectID=1, prestamoID= undefined}
 */
-exports.fk_on_create = function(obj, prestamo) {
+exports.fk_on_create = function (obj, prestamo) {
     var file = './data/usuarios.json';
     var data = fs.readFileSync(file, 'utf8');
     var users = JSON.parse(data);
@@ -54,48 +53,48 @@ exports.fk_on_create = function(obj, prestamo) {
     var file2 = './data/objetos.json';
     var data2 = fs.readFileSync(file2, 'utf8');
     var objects = JSON.parse(data2);
-    
+
     for (var i = 0; i < users.length; i++) {
-        if(users[i].id==obj.userID){
+        if (users[i].id == obj.userID) {
             return true;
         }
     }
     return false;
 }
 
-exports.fk_on_read_one = function(obj, prestamo) {
+exports.fk_on_read_one = function (obj, prestamo) {
     var file = './data/usuarios.json';
     var data = fs.readFileSync(file, 'utf8');
     var users = JSON.parse(data);
-    
+
     for (var i = 0; i < users.length; i++) {
-        if(users[i].id==obj.userID){
+        if (users[i].id == obj.userID) {
             return true;
         }
     }
-    
+
     return false;
 }
 
-exports.fk_on_update = function(obj, prestamo) {
-	var file = './data/usuarios.json';
+exports.fk_on_update = function (obj, prestamo) {
+    var file = './data/usuarios.json';
     var data = fs.readFileSync(file, 'utf8');
     var users = JSON.parse(data);
 
     for (var i = 0; i < users.length; i++) {
-        if(users[i].id==obj.userID){
+        if (users[i].id == obj.userID) {
             return true;
         }
     }
     return false;
 }
 
-exports.fk_on_delete = function(obj, prestamo) {
+exports.fk_on_delete = function (obj, prestamo) {
     var file = './data/usuarios.json';
     var data = fs.readFileSync(file, 'utf8');
     var users = JSON.parse(data);
     for (var i = 0; i < users.length; i++) {
-        if(users[i].id==obj.userID){
+        if (users[i].id == obj.userID) {
             return true;
         }
     }
