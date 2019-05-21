80d26a3948e5b93aea825e91dd12b79c54c785b1
DESKTOP\User <ra.forero11@uniandes.edu.co>
2019-05-20 22:07:33 -0500

    prestamo - get all

--- src/resources/oferta.js
+++ src/resources/oferta.js
@@ -1,24 +1,27 @@
 const Controller = require('../controllers/subresourcesController');
+const exjwt = require('express-jwt');
+const jwtMW = exjwt({
+	secret: 'keyboard cat 4 ever'
+});
 
-module.exports = function(app) {
+module.exports = function (app) {
 	var c = new Controller(require('../models/ofertaModel'), './data/ofertas.json', ['userID', 'ofertaID'], 'userId ');
 
-	app.get('/users/:userID/ofertas',function(req, res)  {
+	app.get('/users/:userID/ofertas', function (req, res) {
 		c.list_all(req, res);
 	})
-	app.post('/users/:userID/ofertas',function(req, res)  {
+	app.post('/users/:userID/ofertas', function (req, res) {
 		c.create(req, res);
 	})
-	app.get('/users/:userID/ofertas/:ofertaID',function(req, res)  {
+	app.get('/users/:userID/ofertas/:ofertaID', function (req, res) {
 		c.read_one(req, res);
 	})
-	app.put('/users/:userID/ofertas/:ofertaID',function(req, res)  {
+	app.put('/users/:userID/ofertas/:ofertaID', function (req, res) {
 		c.update_one(req, res);
 	})
-	app.delete('/users/:userID/ofertas/:ofertaID',function(req, res)  {
+	app.delete('/users/:userID/ofertas/:ofertaID', function (req, res) {
 		c.delete_one(req, res);
 	})
-    
+
 };
 
- 
--- src/resources/prestamo.js
+++ src/resources/prestamo.js
@@ -1,26 +1,26 @@
 const Controller = require('../controllers/subresourcesController');
 const exjwt = require('express-jwt');
 const jwtMW = exjwt({
-  secret: 'keyboard cat 4 ever'
+	secret: 'keyboard cat 4 ever'
 });
 
-module.exports = function(app) {
-	var c = new Controller(require('../models/prestamoModel'), './data/prestamos.json', ['userID', 'prestamoID'],'objectId');
+module.exports = function (app) {
+	var c = new Controller(require('../models/prestamoModel'), './data/prestamos.json', ['userID', 'prestamoID'], 'objectId');
 
-	app.get('/users/:userID/prestamos',function(req, res)  {
+	app.get('/users/:userID/prestamos', jwtMW, function (req, res) {
 		c.list_all(req, res);
 	})
-	app.post('/users/:userID/prestamos',function(req, res)  {
+	app.post('/users/:userID/prestamos', function (req, res) {
 		c.create(req, res);
 	})
-	app.get('/users/:userID/prestamos/:prestamoID',function(req, res)  {
+	app.get('/users/:userID/prestamos/:prestamoID', function (req, res) {
 		c.read_one(req, res);
 	})
-	app.put('/users/:userID/prestamos/:prestamoID',function(req, res)  {
+	app.put('/users/:userID/prestamos/:prestamoID', function (req, res) {
 		c.update_one(req, res);
 	})
-	app.delete('/users/:userID/prestamos/:prestamoID',function(req, res)  {
+	app.delete('/users/:userID/prestamos/:prestamoID', function (req, res) {
 		c.delete_one(req, res);
 	})
-   	
+
 };
